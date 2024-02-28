c6ae63caeb7adab36135efcd72ae1a63d5fa7e6c
Pelle <52049523+farapholch@users.noreply.github.com>
2024-02-28 12:36:24 +0000

    Removal of exaliplus

--- excalidraw-app/components/AppWelcomeScreen.tsx
+++ excalidraw-app/components/AppWelcomeScreen.tsx
@@ -12,29 +12,6 @@ export const AppWelcomeScreen: React.FC<{
   const { t } = useI18n();
   let headingContent;
 
-  if (isExcalidrawPlusSignedUser) {
-    headingContent = t("welcomeScreen.app.center_heading_plus")
-      .split(/(Excalidraw\+)/)
-      .map((bit, idx) => {
-        if (bit === "Excalidraw+") {
-          return (
-            <a
-              style={{ pointerEvents: POINTER_EVENTS.inheritFromUI }}
-              href={`${
-                import.meta.env.VITE_APP_PLUS_APP
-              }?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenSignedInUser`}
-              key={idx}
-            >
-              Excalidraw+
-            </a>
-          );
-        }
-        return bit;
-      });
-  } else {
-    headingContent = t("welcomeScreen.app.center_heading");
-  }
-
   return (
     <WelcomeScreen>
       <WelcomeScreen.Hints.MenuHint>
@@ -43,7 +20,7 @@ export const AppWelcomeScreen: React.FC<{
       <WelcomeScreen.Hints.ToolbarHint />
       <WelcomeScreen.Hints.HelpHint />
       <WelcomeScreen.Center>
-        <img src="TV_Logo_Red.png" alt="Trv_Logo" width={450} height={95}/>
+        <img src="TV_Logo_Red.png" alt="Trv_Logo" width={450} height={95} />
         <WelcomeScreen.Center.Heading>
           {<h4>Välkommen till Rita - Digital whiteboard på Trafikverket</h4>}
         </WelcomeScreen.Center.Heading>
@@ -54,7 +31,7 @@ export const AppWelcomeScreen: React.FC<{
             <WelcomeScreen.Center.MenuItemLiveCollaborationTrigger
               onSelect={() => props.setCollabDialogShown(true)}
             />
-          )}          
+          )}
         </WelcomeScreen.Center.Menu>
       </WelcomeScreen.Center>
     </WelcomeScreen>
