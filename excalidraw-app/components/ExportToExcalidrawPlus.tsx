78a529a72707c91070654c380589954435ee0009
Pelle <52049523+farapholch@users.noreply.github.com>
2024-03-25 09:55:42 +0100

    Excalidraw+ Removal

--- excalidraw-app/components/ExportToExcalidrawPlus.tsx
+++ excalidraw-app/components/ExportToExcalidrawPlus.tsx
@@ -14,7 +14,6 @@ import { encodeFilesForUpload } from "../data/FileManager";
 import { MIME_TYPES } from "../../src/constants";
 import { trackEvent } from "../../src/analytics";
 import { getFrame } from "../../src/utils";
-import { ExcalidrawLogo } from "../../src/components/ExcalidrawLogo";
 import { getStorageBackend } from "../data/config";
 
 export const exportToExcalidrawPlus = async (
@@ -86,40 +85,5 @@ export const ExportToExcalidrawPlus: React.FC<{
   onSuccess: () => void;
 }> = ({ elements, appState, files, onError, onSuccess }) => {
   const { t } = useI18n();
-  return (
-    <Card color="primary">
-      <div className="Card-icon">
-        <ExcalidrawLogo
-          style={{
-            [`--color-logo-icon` as any]: "#fff",
-            width: "2.8rem",
-            height: "2.8rem",
-          }}
-        />
-      </div>
-      <h2>Excalidraw+</h2>
-      <div className="Card-details">
-        {t("exportDialog.excalidrawplus_description")}
-      </div>
-      <ToolButton
-        className="Card-button"
-        type="button"
-        title={t("exportDialog.excalidrawplus_button")}
-        aria-label={t("exportDialog.excalidrawplus_button")}
-        showAriaLabel={true}
-        onClick={async () => {
-          try {
-            trackEvent("export", "eplus", `ui (${getFrame()})`);
-            await exportToExcalidrawPlus(elements, appState, files);
-            onSuccess();
-          } catch (error: any) {
-            console.error(error);
-            if (error.name !== "AbortError") {
-              onError(new Error(t("exportDialog.excalidrawplus_exportError")));
-            }
-          }
-        }}
-      />
-    </Card>
-  );
+  return <Card color="primary"></Card>;
 };
