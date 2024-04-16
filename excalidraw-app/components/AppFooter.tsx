2fd5f839be0bf40a0c20c78134214ab590d6ecea
Pelle <52049523+farapholch@users.noreply.github.com>
2024-04-16 08:53:35 +0200

    NewRitaUrl

--- excalidraw-app/components/AppFooter.tsx
+++ excalidraw-app/components/AppFooter.tsx
@@ -14,7 +14,7 @@ export const AppFooter = React.memo(() => {
       >
         TESTMILJÖ
       </div>
-      <a href="https://wiki.trafikverket.local/display/TAOS/Rita+informationssida">
+      <a href="https://wiki.trafikverket.local/pages/viewpage.action?pageId=113739507">
         <img src="TV_logo_symbol_rgb_rod.png" alt="Trv_Logo" width={22} height={32} title="Hjälp med Rita" />
       </a>
     </Footer>
